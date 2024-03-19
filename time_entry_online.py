cdba3df2dc82f971e79cd0c97dd09386200971c1
Maxime JACOUPY <m.jacoupy@institut-myologie.org>
2024-03-19 17:46:52 +0100

    mm

--- time_entry_online.py
+++ time_entry_online.py
@@ -200,7 +200,7 @@ def load_arc_passwords():
 def main():
     ARC_PASSWORDS = load_arc_passwords()
     st.write(ARC_PASSWORDS.keys())
-    st.set_page_config(layout="wide")
+    # st.set_page_config(layout="wide")
     st.title("I-Motion Adulte - Espace ARCs")
 
     # Authentification de l'utilisateur
