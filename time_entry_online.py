68ee69733d9c1480e29ce51ad765279ada8367bf
Maxime JACOUPY <m.jacoupy@institut-myologie.org>
2024-03-20 10:58:34 +0100

    mm

--- time_entry_online.py
+++ time_entry_online.py
@@ -212,14 +212,14 @@ def load_csv_from_s3(bucket_name, file_name, sep=';', encoding='utf-8'):
 
     return data
 
-# def load_arc_passwords():
-#     try:
-#         # Tentez de charger le fichier avec l'encodage UTF-8
-#         df = load_csv_from_s3(BUCKET_NAME, ARC_PASSWORDS_FILE, sep=';', encoding='utf-8')
-#     except UnicodeDecodeError:
-#         # Si une erreur d'encodage survient, tentez de charger avec l'encodage Latin1
-#         df = load_csv_from_s3(BUCKET_NAME, ARC_PASSWORDS_FILE, sep=';', encoding='latin1')
-#     return dict(zip(df['ARC'], df['MDP']))
+def load_arc_passwords():
+    try:
+        # Tentez de charger le fichier avec l'encodage UTF-8
+        df = load_csv_from_s3(BUCKET_NAME, ARC_PASSWORDS_FILE, sep=';', encoding='utf-8')
+    except UnicodeDecodeError:
+        # Si une erreur d'encodage survient, tentez de charger avec l'encodage Latin1
+        df = load_csv_from_s3(BUCKET_NAME, ARC_PASSWORDS_FILE, sep=';', encoding='latin1')
+    return dict(zip(df['ARC'], df['MDP']))
 
 
 #####################################################################
