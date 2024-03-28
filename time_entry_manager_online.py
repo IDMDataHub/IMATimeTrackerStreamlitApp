13180c897bbca14d1582cb9e72cc05d52833f08f
mjacoupy <maxime.jacoupy@gmail.com>
2024-03-28 11:36:30 +0100

    new of tab 1 and 2

--- time_entry_manager_online.py
+++ time_entry_manager_online.py
@@ -434,11 +434,12 @@ def main():
         with col_modif:
             st.markdown("#### Affectation des études")
             arc_options = arc_df['ARC'].dropna().astype(str).tolist()
+            arc_options = sorted(arc_options) + ['nan']
             for i, row in study_df.iterrows():
                 with st.expander(f"{row['STUDY']}"):
                     # Pour chaque étude, permettez la modification de l'ARC principal, du backup et du mot de passe
-                    new_arc_principal = st.selectbox(f"ARC Principal pour {row['STUDY']}", sorted(arc_options), key=f"principal_{i}")
-                    new_arc_backup = st.selectbox(f"ARC Backup pour {row['STUDY']}", sorted(arc_options), key=f"backup_{i}", help="Optionnel")
+                    new_arc_principal = st.selectbox(f"ARC Principal pour {row['STUDY']}", arc_options, key=f"principal_{i}")
+                    new_arc_backup = st.selectbox(f"ARC Backup pour {row['STUDY']}", arc_options, index=len(arc_options)-1, key=f"backup_{i}", help="Optionnel")
 
                     # Appliquer les modifications directement, cela nécessiterait un bouton de sauvegarde pour chaque étude ou un global après la boucle
                     study_df.at[i, 'ARC'] = new_arc_principal
